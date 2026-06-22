0586fb0907b7d9309653ce13a3bbda01a74b7ffb
BagrinD <bagrin.danu@gmail.com>
2026-06-22 12:36:10 +0300

    Added schemas in database

--- backend/src/main/java/com/amadeus/backend/models/UserEntity.java
+++ backend/src/main/java/com/amadeus/backend/models/UserEntity.java
@@ -11,7 +11,7 @@ import java.util.Collection;
 import java.util.List;
 
 @Entity
-@Table(name = "users")
+@Table(name = "users", schema = "auth")
 @SuperBuilder
 @NoArgsConstructor
 public class UserEntity extends BaseEntity implements UserDetails {
