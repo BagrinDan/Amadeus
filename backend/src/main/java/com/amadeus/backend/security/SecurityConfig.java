ac8cfbfacbb1011d3b432ddc16eeddfdab4f5737
BagrinD <bagrin.danu@gmail.com>
2026-06-28 11:41:27 +0300

    Added smoke test in delploy.yml

--- backend/src/main/java/com/amadeus/backend/security/SecurityConfig.java
+++ backend/src/main/java/com/amadeus/backend/security/SecurityConfig.java
@@ -45,6 +45,7 @@ public class SecurityConfig {
                         .requestMatchers("/",
                                 "/api/version",
                                 "/health",
+                                "/version",
                                 "/swagger-ui/**",
                                 "/swagger-ui.html",
                                 "/v3/api-docs/**",
